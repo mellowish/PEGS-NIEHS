995c124d496343db57c44f89f044e46748f5f53a
Melissa Lowe <melowe1216@gmail.com>
2020-12-16 13:35:07 -0500

    Update TRI_forTaylor.R
    added pegs addresses

--- TRI_forTaylor.R
+++ TRI_forTaylor.R
@@ -1,3 +1,35 @@
+###libraries ###
+
+library(rgdal)
+library(sp)
+library(sf)
+library(rgeos)
+library(tidyr)
+library(dplyr)
+library(ncdf4)           
+library(raster)
+library(reshape2)
+library(ggplot2)
+library(tidyverse)
+library(sp)
+library(rgeos)
+library(nngeo)
+
+###import PEGS addresses ###
+PEGS3 <- read.csv("~/Desktop/PEGS_CensusTractAddresses.csv")
+PEGS2 <- subset(PEGS3, !is.na(PEGS3$geo_latitude))
+
+
+#some subjects have more than 1 address, need to modify our epr number so these can be dealt with
+PEGS2$epr_number_TYPE <- ifelse(PEGS2$geo_study_event == "Exposome Part A - Current Address", paste0(PEGS2$epr_number, "_2"),paste0(PEGS2$epr_number, "_1"))
+
+xy <- PEGS2[,c("geo_longitude","geo_latitude")]
+spdf <- SpatialPointsDataFrame(coords = xy, data = PEGS2, proj4string = CRS("+proj=longlat +datum=NAD83 +no_defs"))
+#CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"))
+spdf2 <- spTransform(spdf,  CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"))
+
+
+
 files.dir <- "~/Desktop/TRI" 
 
 filenames_TRI <- list.files(path=files.dir, full.names=T) 
@@ -46,3 +78,17 @@ write.csv(EB2, file="~/Desktop/TRI/TOLUENE_allyears.csv", row.names=F) #all addr
 
 
 #Turn these into spatial points dataframes
+
+
+names(TOLUENE2) <- c("latitude","longitude","facility", "fugitive_air" ,"stack_air")
+names(BENZENE2) <- c("latitude","longitude","facility", "fugitive_air" ,"stack_air")
+names(XYLENE2) <- c("latitude","longitude","facility", "fugitive_air" ,"stack_air")
+names(EB2) <- c("latitude","longitude","facility", "fugitive_air" ,"stack_air")
+
+
+TOLUENE3 <- st_as_sf(TOLUENE2, coords = c('longitude','latitude'), crs ="+proj=longlat +datum=NAD83 +no_defs")
+
+BENZENE3 <- st_as_sf(BENZENE2, coords = c('longitude','latitude'), crs ="+proj=longlat +datum=NAD83 +no_defs")
+XYLENE3 <- st_as_sf(XYLENE2, coords = c('longitude','latitude'), crs ="+proj=longlat +datum=NAD83 +no_defs")
+EB3 <- st_as_sf(EB2, coords = c('longitude','latitude'), crs ="+proj=longlat +datum=NAD83 +no_defs")
+
